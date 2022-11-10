ce116c4203fa7188e80cbaa35dec8fc848900aa5
SamaraLima16 <117873121+SamaraLima16@users.noreply.github.com>
2022-11-10 18:26:47 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const menu_btn = document.querySelector('.hamburguer')
+const mobile_menu = document.querySelector('.mobile-nav')
+
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active')
+    mobile_menu.classList.toggle('is-active')
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
